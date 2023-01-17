9a287b930b9c9409c9d2b1938b082e490eb388b8
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-17 14:12:56 +0200

    add agents

--- src/pages/ai.tsx
+++ src/pages/ai.tsx
@@ -4,11 +4,6 @@ import { Chat } from "../components/Chat";
 function Home() {
   return (
     <Page className="flex flex-col gap-12">
-      <section className="flex flex-col items-center gap-6">
-        <Text variant="h1">OpenAI GPT-3</Text>
-        <Text className="text-zinc-600">An A.I using GPT-3</Text>
-      </section>
-
       <section className="flex flex-col items-center gap-3">
         <Text variant="h2">Chat</Text>
         <div className="lg:w-2/3">
