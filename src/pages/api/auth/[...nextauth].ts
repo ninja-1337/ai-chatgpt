028e9210be5355e55084a7c2d04d4cc31942635f
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-17 17:06:08 +0200

    add auth and db tables

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -23,6 +23,10 @@ export const authOptions: NextAuthOptions = {
       clientId: env.DISCORD_CLIENT_ID,
       clientSecret: env.DISCORD_CLIENT_SECRET,
     }),
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
     // ...add more providers here
   ],
 };
