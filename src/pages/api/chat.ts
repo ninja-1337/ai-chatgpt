3b5775384fad33deb5976c344fa652e0fdf1997a
ninja-1337 <evangelou.e.95@gmail.com>
2023-01-28 00:18:22 +0200

    more agents

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -79,7 +79,7 @@ export default async function handler(req: any, res: any) {
   const payload = {
     model: "text-davinci-003",
     prompt: finalPrompt,
-    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.3,
+    temperature: process.env.AI_TEMP ? parseFloat(process.env.AI_TEMP) : 0.7,
     max_tokens: process.env.AI_MAX_TOKENS
       ? parseInt(process.env.AI_MAX_TOKENS)
       : 200,
