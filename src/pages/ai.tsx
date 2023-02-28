9fa3f09c7e96944855cfafbf8cbfe866aa9b3e05
ninja-1337 <evangelou.e.95@gmail.com>
2023-02-28 13:51:08 +0200

    add auth for ai

--- src/pages/ai.tsx
+++ src/pages/ai.tsx
@@ -1,15 +1,25 @@
 import { Layout, Text, Page } from "@vercel/examples-ui";
 import { Chat } from "../components/Chat";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 function Home() {
+  const { data: session, status } = useSession();
+
   return (
     <Page className="flex flex-col gap-12">
-      <section className="flex flex-col items-center gap-3">
-        <Text variant="h2">Chat</Text>
-        <div className="lg:w-2/3">
-          <Chat />
-        </div>
-      </section>
+      {!session && <p>Please login to use App</p>}
+      {session && (
+        <>
+          <section className="flex flex-col items-center gap-6">
+            <Text variant="h1">Verbal Agents</Text>
+          </section>
+          <section className="flex flex-col items-center gap-3">
+            <Text variant="h2">Chat</Text>
+            <div className="lg:w-2/3">
+              <Chat />
+            </div>
+          </section>
+        </>
+      )}
     </Page>
   );
 }
