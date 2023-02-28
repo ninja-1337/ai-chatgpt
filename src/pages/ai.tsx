ccbefd1331afb72dd80628462e34b18b38542242
ninja-1337 <evangelou.e.95@gmail.com>
2023-02-28 14:04:56 +0200

    add auth for ai

--- src/pages/ai.tsx
+++ src/pages/ai.tsx
@@ -6,7 +6,11 @@ function Home() {
 
   return (
     <Page className="flex flex-col gap-12">
-      {!session && <p>Please login to use App</p>}
+      {!session && (
+        <section className="flex flex-col items-center gap-6">
+          <Text variant="h1">Please login to use App</Text>
+        </section>
+      )}
       {session && (
         <>
           <section className="flex flex-col items-center gap-6">
