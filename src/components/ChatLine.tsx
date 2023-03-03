7d67b7278334e8f7ea1f95d8f9effec36b82b1cb
ninja-1337 <evangelou.e.95@gmail.com>
2023-03-03 04:02:30 +0200

    gradient messages

--- src/components/ChatLine.tsx
+++ src/components/ChatLine.tsx
@@ -66,8 +66,8 @@ export function ChatLine({ who = "bot", message }: Message) {
           className={clsx(
             "text ",
             who == "bot"
-              ? "font- float-right mb-5 rounded-xl from-orange-500 to-yellow-100  px-4 py-5 font-semibold shadow-xl ring-1 ring-zinc-100 sm:px-6"
-              : "text-white-400 bg-gradient-to-r from-orange-500 to-yellow-100 ",
+              ? "  float-right mb-5  rounded-xl px-4 py-5 font-semibold shadow-xl ring-1 sm:px-6"
+              : "text-white-400   ",
             who == "user"
               ? "font- float-right mb-5 rounded-xl  from-cyan-400 to-blue-100 px-4 py-5 font-semibold shadow-lg ring-1 ring-zinc-100 sm:px-6"
               : "font- float-right mb-5 rounded-xl  from-cyan-400 to-blue-100 px-4 py-5 font-semibold shadow-lg ring-1 ring-zinc-100 sm:px-6"
