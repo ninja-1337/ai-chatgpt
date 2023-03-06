8f171951aeff813378e28bcff5657e1831b74da6
ninja-1337 <evangelou.e.95@gmail.com>
2023-03-07 00:04:20 +0200

    Agent querried from DB

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -59,8 +59,8 @@ export default async function handler(req: any, res: any) {
     },
   });
 
-  const Prmpt = agent;
-  res.status(200).json({ text: Prmpt });
+  const Prmpt = agent.prompt;
+
   // if (agent === "CA") {
   //   x = "You respond only with code for the given task prompted";
   // } else if (agent === "LEA") {
