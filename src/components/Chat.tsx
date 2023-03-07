7c2167c40345335426b4845afa062377da4ed07c
ninja-1337 <evangelou.e.95@gmail.com>
2023-03-07 22:22:51 +0200

    Agent querried from DB

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -15,6 +15,31 @@ export const initialMessages: Message[] = [
   },
 ];
 
+const Agent = ({ input, setInput, sendMessage }: any) => (
+  <div className="clear-both mt-6 flex w-11/12">
+    <input
+      type="text"
+      aria-label="chat input"
+      required
+      className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm"
+      value={input}
+      onChange={(e) => {
+        setInput(e.target.value);
+      }}
+    />
+    <Button
+      type="submit"
+      className="ml-4 flex-none"
+      onClick={() => {
+        sendMessage(input);
+        setInput("");
+      }}
+    >
+      Add Agent
+    </Button>
+  </div>
+);
+
 const InputMessage = ({ input, setInput, sendMessage }: any) => (
   <div className="clear-both mt-6 flex w-11/12">
     <input
@@ -116,10 +141,9 @@ export function Chat() {
 
   return (
     <div>
-      Agent Name:{" "}
-      <InputMessage input={newAgentName} setInput={setNewAgentName} />
+      Agent Name: <Agent input={newAgentName} setInput={setNewAgentName} />
       Agent Prompt:
-      <InputMessage input={newAgentPrompt} setInput={setNewAgentPrompt} />
+      <Agent input={newAgentPrompt} setInput={setNewAgentPrompt} />
       <Select
         onChange={(state) => {
           setAgent(state as any);
