8a2181e637256debdbfe1645237a4dacf553cb08
unknown <you@example.com>
2023-05-04 20:21:07 +0300

    Add dark theme

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -239,7 +239,7 @@ export function Chat() {
         className={theme=="dark" ? " text-black bg-gray-700" : "clear-both mx-4"}
       />
     
-        <div className="rounded-2xl border-zinc-100 lg:border lg:p-6">
+        <div className="h-full rounded-2xl border-zinc-100 lg:border lg:p-6">
           {messages.map(({ message, who }, index) => (
             <>
               <ChatLine key={index} who={who} message={message} theme={theme}/>
